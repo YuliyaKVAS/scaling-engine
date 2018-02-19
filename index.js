f7aba8b39273ebbc592353230b8fbb7706ff1789
Yuliya <yuliya2095@bk.ru>
2018-02-19 22:59:22 +0300

    add button functionality(add new list item)

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ var initItems = function($items) {
 $( function() {
     $("#btn").click(function(){
         if($("#inputdefault").val()!= ''){
-            var $newItemElement = $('<li class="list-group-item">'+$("#inputdefault").val()+'<button class="delete" style="width:21px;height:21px;border:0">-</button>'+'</li>')//Create new element using jQuery
+            var $newItemElement = $('<li class="list-group-item">'+$("#inputdefault").val()+'<button class="btn btn-default btn-sm">-</button>'+'</li>')//Create new element using jQuery
             initItems($newItemElement)//We have to bind click to newly created item, that is why it was not working before
             $('#list').append($newItemElement);//Append new element to the list
             $("#inputdefault").val("")// After adding newItemElement clear textbox
 
-            $(".delete").on("click", function(){
+            $(".btn-default").on("click", function(){
             var $parent = $(this).parent();
 
             setTimeout(function(){
